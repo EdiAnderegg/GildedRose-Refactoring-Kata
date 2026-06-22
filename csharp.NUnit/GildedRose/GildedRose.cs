419549db65743d77b7faf51872041fa503959988
EdiAnderegg <93870106+EdiAnderegg@users.noreply.github.com>
2026-06-22 11:35:38 +0200

    1. Tests were added to check the business rules. 2. Added the feature category 'Conjured Items'. 3. Changed the approval test for conjured items so that they meet the expected business rules.

--- csharp.NUnit/GildedRose/GildedRose.cs
+++ csharp.NUnit/GildedRose/GildedRose.cs
@@ -1,6 +1,45 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace GildedRoseKata;
+
+
+/*
+ * 
+ * 
+ * 1. Understand the Rules
+ * What is really needed ?
+ * 
+ * 2. Understand the Code
+ * In order to understand the code and see if everything works
+ * as expected my approach will be, Write test first that test the logic of the Business Rules. 
+ * 
+ * So my approach was write the business rules like a "checklist" in a paper go and check one rule
+ * at a time with a test and repeat. 
+ * 
+ * This wil help alot to understand what is actually happening in the Code
+ * and also I will be able to find Bugs in the System, if any.
+ * 
+ * 
+ * 3. Now that I have all tests Cases that cover the needed Business Rules,
+ * 
+ *    - First before changing the messi code I will try to add conjure items logic and make the test pass.
+ *    
+ *    - Then i will make small changes to the if statments, like try to separate the 
+ *    if statements to own Items Categories
+ * 
+ * 
+ * 
+ */
+
+public static class ItemCategory
+{
+    public static readonly string Normal = "";
+    public static readonly string Sulfuras = "Sulfuras, Hand of Ragnaros";
+    public static readonly string AgedBrie = "Aged Brie";
+    public static readonly string BackstagePasses = "Backstage passes to a TAFKAL80ETC concert";
+    public static readonly string Conjured = "Conjured Mana Cake";
+}
 
 public class GildedRose
 {
@@ -11,10 +50,47 @@ public class GildedRose
         this.Items = Items;
     }
 
+
+    public Item GetItemByIndex(int index)
+    {
+        return Items[index];
+    }
+
     public void UpdateQuality()
     {
+
         for (var i = 0; i < Items.Count; i++)
         {
+
+
+            // Item Properties
+            string itemName = Items[i].Name;
+            int itemSellIn = Items[i].SellIn;
+            int itemQuality = Items[i].Quality;
+
+            // Item Rules
+            int MinSellInValue = 0;
+            int MinQualityValue = 0;
+            int MaxQualityValue = 50;
+
+
+            // Category: Conjured Items
+            if (itemName == ItemCategory.Conjured)
+            {
+                // Check Rules
+                if (itemQuality > MaxQualityValue) Items[i].Quality = MaxQualityValue;
+
+                //SellIn Rules
+                Items[i].SellIn = itemSellIn - 1;
+
+                // Quality Rules
+                // Conjured Items Degrade twice as fast as Normal Items
+                if (itemQuality <= MinQualityValue) Items[i].Quality = MinQualityValue;
+                else Items[i].Quality = itemQuality - 2;
+
+                break;
+            }
+
             if (Items[i].Name != "Aged Brie" && Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
             {
                 if (Items[i].Quality > 0)
@@ -51,6 +127,8 @@ public class GildedRose
                     }
                 }
             }
+
+
 
             if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
             {
--- csharp.NUnit/GildedRoseTests/GildedRoseTest.cs
+++ csharp.NUnit/GildedRoseTests/GildedRoseTest.cs
@@ -6,12 +6,85 @@ namespace GildedRoseTests;
 
 public class GildedRoseTest
 {
-    [Test]
-    public void Foo()
+
+
+    // TestCases of Category: Normal
+    [TestCase("NormalItem", 10 ,10, 9)]
+
+    // TestCases of Category: Aged Brie
+    [TestCase("Aged Brie", 10, 8, 9)]
+
+    // TestCases of Category: Sulfuras
+    [TestCase("Sulfuras, Hand of Ragnaros", 0, 80, 0)]
+    [TestCase("Sulfuras, Hand of Ragnaros", -1, 80, -1)]
+    public void UpdateQuality_Should_Update_Item_SellIn(
+        string name,
+        int sellIn,
+        int quality,
+        int expectedSellIn)
     {
-        var items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 0 } };
+        // Arrange
+        var item = new Item
+        {
+            Name = name,
+            SellIn = sellIn,
+            Quality = quality
+        };
+
+        var items = new List<Item> { item };
         var app = new GildedRose(items);
+
+        // Act
         app.UpdateQuality();
-        Assert.That(items[0].Name, Is.EqualTo("fixme"));
+
+        // Assert
+        Assert.That(item.SellIn, Is.EqualTo(expectedSellIn));
     }
+
+    // TestCases of Category: Normal
+    [TestCase("NormalItem", 10, 8, 7)]
+    [TestCase("NormalItem", 0, 8, 6)]
+    [TestCase("NormalItem", 10, 0, 0)]
+    [TestCase("NormalItem", 10, 51, 50)]
+
+    // TestCases of Category: Aged Brie
+    [TestCase("Aged Brie", 10, 8, 9)]
+    [TestCase("Aged Brie", 10, 50, 50)]
+
+    // TestCases of Category: Sulfuras
+    [TestCase("Sulfuras, Hand of Ragnaros", 0, 80, 80)]
+    [TestCase("Sulfuras, Hand of Ragnaros", -1, 80, 80)]
+
+    // TestCases of Category: Backstage passes
+    [TestCase("Backstage passes to a TAFKAL80ETC concert", 10, 20, 22)]
+    [TestCase("Backstage passes to a TAFKAL80ETC concert", 5, 20, 23)]
+    [TestCase("Backstage passes to a TAFKAL80ETC concert", 0, 20, 0)]
+
+    // TestCases of Category: Conjured
+    [TestCase("Conjured Mana Cake", 10, 20, 18)]
+
+    public void UpdateQuality_Should_Update_Item_Quality(
+    string name,
+    int sellIn,
+    int quality,
+    int expectedQuality)
+    {
+        // Arrange
+        var item = new Item
+        {
+            Name = name,
+            SellIn = sellIn,
+            Quality = quality
+        };
+
+        var items = new List<Item> { item };
+        var app = new GildedRose(items);
+
+        // Act
+        app.UpdateQuality();
+
+        // Assert
+        Assert.That(item.Quality, Is.EqualTo(expectedQuality));
+    }
+
 }
